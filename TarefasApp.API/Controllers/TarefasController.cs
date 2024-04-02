fa0b57751c4c30be3d2443c14a5fd3df96a5f71e
sergio-coti <cotiaulajava@outlook.com>
2024-04-02 08:51:15 -0300

    first commit

---
+++ TarefasApp.API/Controllers/TarefasController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TarefasApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TarefasController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Put()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ TarefasApp.API/Extensions/SwaggerDocExtension.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace TarefasApp.API.Extensions
+{
+    /// <summary>
+    /// Classe de extensão para configuração do Swagger
+    /// </summary>
+    public static class SwaggerDocExtension
+    {
+        /// <summary>
+        /// Método de extensão para configurar as preferências 
+        /// da documentação da API gerada pelo Swagger.
+        /// </summary>
+        public static IServiceCollection AddSwaggerDoc(this IServiceCollection services)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(options => options.SwaggerDoc("v1", new OpenApiInfo 
+            { 
+                Title = "TarefasApp - Treinamento C# Avançado Formação Arquiteto",
+                Description = "API para controle de tarefas de usuários",
+                Version = "1.0",
+                Contact = new OpenApiContact
+                {
+                    Name = "COTI Informática",
+                    Email = "contato@cotiinformatica.com.br",
+                    Url = new Uri("http://www.cotiinformatica.com.br")
+                }
+            }));
+
+            return services;
+        }
+
+        /// <summary>
+        /// Método de extensão para executar as configurações do Swagger
+        /// </summary>
+        public static IApplicationBuilder UseSwaggerDoc(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "TarefasApp");
+            });
+
+            return app;
+        }
+    }
+}
